5880493d6e6fe0e16df4ab253b115f3ed577498f
Adil <satilganov.adil@gmail.com>
2026-02-20 09:45:56 +0500

    fix errors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,9 +5,6 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { registerLocaleData } from '@angular/common';
-import { provideStore } from '@ngrx/store';
-import { userReducer } from './component/users-list/store/user.reducer';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 registerLocaleData(localeRu);
 
@@ -17,9 +14,5 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideAnimationsAsync(),
     { provide: LOCALE_ID, useValue: 'ru-RU' },
-    provideStore({
-      users: userReducer,
-    }),
-    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
   ],
 };
